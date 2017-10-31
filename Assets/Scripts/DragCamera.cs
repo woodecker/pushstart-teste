c983fdbf0dfa25919334bf472d0aed0870e77730
Pedro Costa <woodecker@gmail.com>
2017-10-31 19:03:38 -0300

    Mouse position

--- Assets/Scripts/DragCamera.cs
+++ Assets/Scripts/DragCamera.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DragCamera : MonoBehaviour {
 
+	private Vector3 startPos;
 
 	public float zPosition = -10f;
 
@@ -13,9 +14,14 @@ public class DragCamera : MonoBehaviour {
 			return;
 
 		if (Input.GetMouseButtonDown (0)) {
+			startPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			startPos.z = zPosition;
 		}
 
 		if (Input.GetMouseButton (0)) {
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			mousePos.z = zPosition;
+			transform.position += startPos - mousePos;
 		}
 	}
 }
