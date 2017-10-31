1e293be782e449b839a8d0f56724db9dc2fd05d3
Pedro Costa <woodecker@gmail.com>
2017-10-31 17:13:37 -0300

    UI show cost building

--- Assets/Scripts/ButtonBuilding.cs
+++ Assets/Scripts/ButtonBuilding.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 public class ButtonBuilding : MonoBehaviour {
 
 	public GameObject building;
+	public Text costText;
+
+	void Start(){
+		costText.text = building.GetComponent<Building> ().cost.ToString();
+	}
 
 	void Update(){
 		GetComponent<Button> ().interactable = (building.GetComponent<Building> ().cost <= Player.instance.Money);
